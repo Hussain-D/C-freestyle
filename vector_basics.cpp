36cade5819cefb770901310dda80217bea884fec
Hussain-D <dudekulahussain86@gmail.com>
2023-01-17 12:28:31 +0530

    Vector basics

---
+++ vector_basics.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    // Initialization methods
+    vector <int> vector1 = {1,2,3,4,5};
+    vector <int> vector2 {1,2,3,4,5};
+    vector <int> vector3(5,2); //equal to {2,2,2,2,2}
+    return 0;
+}
